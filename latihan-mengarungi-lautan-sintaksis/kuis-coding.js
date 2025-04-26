bf8e9bdc92dc916b54f90727dde7ce44c3742aee
asadayoo <ahmadassadf428@gmail.com>
2025-04-26 16:38:26 +0700

    menambahkan komentar

--- latihan-mengarungi-lautan-sintaksis/kuis-coding.js
+++ latihan-mengarungi-lautan-sintaksis/kuis-coding.js
@@ -1,7 +1,12 @@
+// Deklarasi variabel currency dengan nilai "IDR"
 const currency = "IDR";
+// Deklarasi variabel value dengan nilai 10000
 let value = 10000;
+// Menambahkan 5000 ke nilai value
 value += 5000
 
+// Menggabungkan currency dan value menjadi satu string
 const money = currency + " " + value;
 
+// Menampilkan hasil ke console
 console.log(money);
